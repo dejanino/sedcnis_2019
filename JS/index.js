2c8daf6c82a855d1f1804bb67fcaf0b9176371ec
Ninoslav Ciric <ninoslav.ciric89@gmail.com>
2020-02-12 19:02:55 +0100

    Ubacen slidedown bar

--- JS/index.js
+++ JS/index.js
@@ -1,19 +1,22 @@
 let firstImage = document.querySelector('.background-picture')
 let x = document.querySelector('.h3')
 let paragraph = document.querySelector('.paragraph-slider')
+let anchor = document.querySelector('.anchor')
 let slider = [{img:"images/slider-image1.jpg",
                tittle: "Something writtten",
-               description: "Something written in paragraphs"
-               
+               description: "Something written in paragraphs",
+               url: "#footer"
+
               },             
               {img:"images/slider-image2.jpg",
                tittle: "Something but other",
-               description: "Bla blsahdada sadadas"
-               
+               description: "Bla blsahdada sadadas",
+               url: "#footer"
               },
               {img:"images/slider-image3.jpg",
                tittle: "Something but saassa",
-               description: "awsadkasif adfaf"
+               description: "awsadkasif adfaf",
+               url: "#footer"
               }
             ]
 
@@ -33,17 +36,11 @@ function changeSlide(aindex){
     firstImage.src = slider[imageIndex].img;
     x.innerHTML = slider[imageIndex].tittle;
     paragraph.innerHTML = slider[imageIndex].description;
-    // if (imageIndex < slider.length -1){
-    //     imageIndex = imageIndex + aindex;
-    // }
-    // else if (imageIndex < 0){
-    //     imageIndex = slider.length -1;
-    // }
-    // else {
-    //     imageIndex = 0;
-    // }
+    anchor.href = slider[imageIndex].url;
+
     console.log(imageIndex)
 }
+changeSlide(0)
 
 dugmeDesno.addEventListener('click', function(){
     changeSlide(1);
@@ -66,3 +63,19 @@ function changeSlideLevo(){
         
         
         */
+       let navigationBar = document.querySelector('.nav'); 
+       //funkcija za gasenje i paljenje
+       let dropDownMenu = function() {
+           let navBarInvisible = navigationBar.classList.contains('prikazan-nav');
+       
+           if (navBarInvisible) {
+               navigationBar.classList.remove('prikazan-nav');
+            
+           }
+           else {
+               navigationBar.classList.add('prikazan-nav');
+               
+           }
+       }
+       let anchorClick = document.querySelector('.drop-down-menu');
+       anchorClick.addEventListener('click', dropDownMenu); 
