f4e4d254b19f787e1118b57253720e2e9dea5056
Crassmof <strax991@gmail.com>
2020-02-27 11:23:02 +0100

    Strale sredjivao, slobodno obrisi kad shvatis :)

---
+++ projekat-popularcourses-strale.js
@@ -0,0 +1,327 @@
+function main () {
+
+    
+    var coursesSlider = [
+        {
+            image: 'images/courses-image1.jpg' ,
+            title: 'Bussines & Management',
+            description : 'Lorem',
+            avatarImage : 'images/author-image1.jpg',
+            name: 'Mark'
+        },
+        
+        {
+            image: 'images/courses-image2.jpg' ,
+            title: 'Bussines & Bussines',
+            description : 'Borem',
+            avatarImage : 'images/author-image2.jpg',
+            name: 'Jesica'
+        },
+        
+        {
+            image: 'images/courses-image3.jpg' ,
+            title: 'Management & Bussines',
+            description : 'Smorem',
+            avatarImage : 'images/author-image3.jpg',
+            name: 'Nicole'
+        },
+
+        {
+            image: 'images/courses-image4.jpg' ,
+            title: 'Management & Management',
+            description : 'Vlorem',
+            avatarImage : 'images/author-image4.jpg',
+            name: 'Daniel'
+        },
+
+        { 
+             image: 'images/courses-image5.jpg' ,
+             title: 'Management ',
+             description : 'Glorem',
+             avatarImage : 'images/tst-image1.jpg',
+             name: 'John'
+    
+        }
+
+    ]
+
+    let isMobile = false;
+    let counter = 0;
+    // let counter1 = 0;
+    // treba da pratis 3 indeksa 
+    let counters = [0, 1, 2];
+
+    let previous = document.querySelector('#previous-arrow');
+    let next  = document.querySelector('#next-arrow');
+
+    let mobilePicture = document.querySelector('#mobile-image');
+    let mobileText = document.querySelector('#mobile-courses-text');
+    let mobileDescription = document.querySelector('#mobile-courses-description');
+    let avatarPicture = document.querySelector('#avatar-image');
+    let avatarName = document.querySelector('#avatar-name');
+
+    // ovo su sad arrays gde je prva slika coursesImage[0], druga slika coursesImage[1] i treca coursesImage[2]
+    let coursesImage = document.getElementsByClassName('course-image');
+    let coursesText = document.getElementsByClassName('course-title');
+    let coursesDescription = document.getElementsByClassName('courses-description');
+    let coursesAvatar = document.getElementsByClassName('image-avatar');
+    let nameAvatar = document.getElementsByClassName('avatar-name');
+    // let titleImage = document.getElementsByClassName('mobile-courses-text');
+    console.log(coursesText);
+    console.log(coursesImage);
+    console.log(coursesDescription);
+    console.log(coursesAvatar);
+    console.log(nameAvatar);
+
+
+let checkSize = function (){
+    if (window.innerWidth > 992 ){
+        isMobile = false;
+        console.log(isMobile);
+        
+    }else {
+        isMobile = true;
+        console.log(isMobile);
+    }
+}
+
+window.addEventListener("resize", checkSize);
+
+previous.addEventListener('click', function(){
+    // console.log('nazad');
+    if(isMobile){
+    
+        if (counter === 0){
+            counter = coursesSlider.length - 1;
+            console.log(counter);
+        }else {
+            counter --;
+            console.log(counter);
+        }
+        mobileChanger();
+
+    }else {     
+        // ovo treba da se izvrsi za sva tri pracena indeksa is "courses"
+        for (let i = 0; i < counters.length; i++) {
+            if (counters[i] === 0){
+                counters[i] = coursesSlider.length - 1;
+            }else {
+                counters[i] --;
+            }
+            //prosledimo index ovoj funkciji da zna u koji od tri kontejnera smesta koji objekat 
+            desktopChanger(i);
+        }
+        
+        // ovo sve moze u jednu funkciju
+        
+        // changePicture(coursesImage,counter1);
+        // changeText(coursesText,counter1);
+        // changeTextDescription(coursesDescription,counter1);
+        // changePictureAvatar(coursesAvatar,counter1);
+        // changeNameAvarat(nameAvatar,counter1);
+
+        // changeSlider(counter1);
+        //  changeTitle(counter1);
+    }
+
+});
+
+next.addEventListener('click',function(){
+   // console.log('napred');
+
+   if(isMobile){
+       
+       if (counter === coursesSlider.length - 1 ){
+           counter = 0 ;
+           console.log(counter);
+        }else {
+            counter ++;
+            console.log(counter);
+        }
+        
+        mobileChanger();
+
+    }else {
+        for (let i = 0; i < counters.length; i++) {
+            if (counters[i] === coursesSlider.length - 1){
+                counters[i] = 0;
+            }else {
+                counters[i] ++;
+            }
+            desktopChanger(i);
+        }
+        
+        //  changePicture(coursesImage,counter1);
+        //  changeText(coursesText,counter1);
+        //  changeTextDescription(coursesDescription,counter1);
+        //  changePictureAvatar(coursesAvatar,counter1);
+        //  changeNameAvarat(nameAvatar,counter1);
+        //  changeSlider(counter1);
+        //  changeTitle(counter1);
+    }
+
+});
+
+function mobileChanger (){
+
+    mobilePicture.src = coursesSlider[counter].image;
+    mobileText.textContent = coursesSlider[counter].title;
+    mobileDescription.textContent = coursesSlider[counter].description;
+    avatarPicture.src = coursesSlider[counter].avatarImage;
+    avatarName.textContent = coursesSlider[counter].name;
+}
+// ovako deklarisemo funkcije!
+let desktopChanger = function (i) {
+    // od 3 slike jednoj po jednoj dodeljuje vrednost iz coursesSlider
+    // ako je i = 0 onda je counters[i] vrednost prvog brojaca (od 3), a ta vrednost ce da odredi koji od objekata iz coursesSlider pozivamo
+    coursesImage[i].src = coursesSlider[counters[i]].image;
+    coursesText[i].textContent = coursesSlider[counters[i]].title;
+    coursesDescription[i].textContent = coursesSlider[counters[i]].description;
+    coursesAvatar[i].src = coursesSlider[counters[i]].avatarImage;
+    nameAvatar[i].textContent = coursesSlider[counters[i]].name;
+}
+
+function changePicture (image, counter){
+    
+     image[0].src = coursesSlider[counter1].image;
+
+    if (counter1 === coursesSlider.length - 2){
+        image[1].src = coursesSlider[1 + counter1].image;
+        image[2].src = coursesSlider[0].image;
+
+    }else if (counter1 === coursesSlider.length - 1){
+        image[1].src = coursesSlider[0].image;
+        image[2].src = coursesSlider[1].image
+
+    }else {
+      //  image[0].src = coursesSlider[0 + counter1].image;
+        image[1].src = coursesSlider[1 + counter1].image;
+        image[2].src = coursesSlider[2 + counter1].image;
+    }
+}
+
+function changeText (text, counter){
+    
+    text[0].textContent = coursesSlider[0 + counter].title;
+
+    if (counter === coursesSlider.length - 2){
+        text[1].textContent = coursesSlider[1 + counter].title;
+        text[2].textContent = coursesSlider[0].title;
+
+    }else if (counter === coursesSlider.length - 1){
+        text[1].textContent = coursesSlider[0].title;
+        text[2].textContent = coursesSlider[1].title;
+
+    }else {
+       // text[0].textContent = coursesSlider[0 + counter].title;
+        text[1].textContent = coursesSlider[1 + counter].title;
+        text[2].textContent = coursesSlider[2 + counter].title;
+    }
+
+}
+
+
+function changeTextDescription (text, counter){
+    
+     text[0].textContent = coursesSlider[0 + counter].title;
+ 
+     if (counter === coursesSlider.length - 2){
+         text[1].textContent = coursesSlider[1 + counter].description;
+         text[2].textContent = coursesSlider[0].description;
+ 
+     }else if (counter === coursesSlider.length - 1){
+         text[1].textContent = coursesSlider[0].description;
+         text[2].textContent = coursesSlider[1].description;
+ 
+     }else {
+       //  text[0].textContent = coursesSlider[0 + counter].description;
+         text[1].textContent = coursesSlider[1 + counter].description;
+         text[2].textContent = coursesSlider[2 + counter].description;
+     }
+ 
+ }
+
+function changePictureAvatar (image, counter1){
+
+    image[0].src = coursesSlider[0 + counter1].avatarImage;
+
+    if (counter1 === coursesSlider.length - 2){
+        image[1].src = coursesSlider[1 + counter1].avatarImage;
+        image[2].src = coursesSlider[0].avatarImage;
+
+    }else if (counter1 === coursesSlider.length - 1){
+        image[1].src = coursesSlider[0].avatarImage;
+        image[2].src = coursesSlider[1].avatarImage;
+
+    }else {
+      //  image[0].src = coursesSlider[0 + counter1].avatarImage;
+        image[1].src = coursesSlider[1 + counter1].avatarImage;
+        image[2].src = coursesSlider[2 + counter1].avatarImage;
+    }
+}
+
+function changeNameAvarat (text, counter){
+    
+     text[0].textContent = coursesSlider[0 + counter].name;
+ 
+     if (counter === coursesSlider.length - 2){
+         text[1].textContent = coursesSlider[1 + counter].name;
+         text[2].textContent = coursesSlider[0].name;
+ 
+     }else if (counter === coursesSlider.length - 1){
+         text[1].textContent = coursesSlider[0].name;
+         text[2].textContent = coursesSlider[1].name;
+ 
+     }else {
+        //  text[0].textContent = coursesSlider[0 + counter].name;
+         text[1].textContent = coursesSlider[1 + counter].name;
+         text[2].textContent = coursesSlider[2 + counter].name;
+     }
+ 
+ }
+
+// function changeSlider (counter){
+
+//     coursesImage[0].src = coursesSlider[0 + counter1].image;
+
+//         if (counter1 === coursesSlider.length - 2){
+//             coursesImage[1].src = coursesSlider[1 + counter1].image;
+//             coursesImage[2].src = coursesSlider[0].image;
+
+//         }else if (counter1 === coursesSlider.length - 1){
+//             coursesImage[1].src = coursesSlider[0].image;
+//             coursesImage[2].src = coursesSlider[1].image
+
+//         }else {
+//             coursesImage[1].src = coursesSlider[1 + counter1].image;
+//             coursesImage[2].src = coursesSlider[2 + counter1].image;
+//         }
+
+// };
+
+// function changeTitle (counter){
+    
+//     coursesText[0].textContent = coursesSlider[0 + counter1].title;
+        
+
+//         if (counter1 === coursesSlider.length - 2){
+//             coursesText[1].textContent = coursesSlider[1 + counter1].title;
+//             coursesText[2].textContent = coursesSlider[0].title;
+
+//         }else if (counter1 === coursesSlider.length - 1){
+//             coursesText[1].textContent = coursesSlider[0].title;
+//             coursesText[2].textContent = coursesSlider[1].title;
+
+//         }else {
+//             coursesText[1].textContent = coursesSlider[1 + counter1].title;
+//             coursesText[2].textContent = coursesSlider[2 + counter1].title;
+//         }
+
+// };
+
+
+
+
+};
+
+main();
