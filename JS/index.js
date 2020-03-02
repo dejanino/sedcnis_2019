94a93e4c61b898c97bcc8b1cedec428947771b97
Ninoslav Ciric <ninoslav.ciric89@gmail.com>
2020-03-02 20:47:13 +0100

    working with placeholders

--- JS/index.js
+++ JS/index.js
@@ -267,6 +267,8 @@ forThumbnail.forEach(function(elementContent, index){
 })
 
 //ZA TEACHER 
+let placeHolderDivTeachers = document.getElementById('teachers-container');
+let markUp = "";
 let request2 = new Request("http://pushadmin.dropmind.com/api/teachers")
 fetch(request2)
 .then(function(responseTeacher) {
@@ -275,27 +277,30 @@ fetch(request2)
         return responseTeacher.json();
     }
     else{
-        alert("Error, no sliders to show");
+        alert("Error, no teachers to show");
     }
 })
 .then(function(teacher){
     console.log(teacher)
-    let placeHolderDivTeachers = document.getElementById('teachers-container');
-    let markUp = "";
-    teacher.forEach(element => {
+    teacher.forEach(element => { //PLACEHOLDER
         markUp+= `
         <div class="teacher">
                 <div class="teachers-image">
-                    <img src= ${element.avatarurl} alt="image" />
+                    <img src= ${element.avatarurl} alt="image" /> 
                 </div>
                 <div class="teachers-text">
                     <h2 class="teachers-heading">${element.name} </h2>
                     <p class="teachers-paragraph">${element.slogan} </p>
                 </div>
                 <div class="teachers-icons">
-                    <a class="teacher-icon" href="#"><i class="fab fa-facebook-square"></i></a>
-                    <a class="teacher-icon" href="#"><i class="fab fa-twitter"></i></a>
-                    <a class="teacher-icon" href="#"><i class="fab fa-instagram"></i></a>
+                    ${ element.twitter !== "" ?
+                        '<a class="teacher-icon" href="#"><i class="fab fa-twitter-square"></i></a>'
+                        : "" 
+                    }
+                    ${ element.facebook !== "" ?
+                    '<a class="teacher-icon" href="#"><i class="fab fa-facebook-square"></i></a>'
+                    : "" 
+                }
                 </div>
             </div>`;
     })
